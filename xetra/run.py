527e151dd700b4ba23cb4680456428804f232be5
Leighton Tyler <leighton@tempostorm.com>
2022-03-08 09:47:53 -0500

    added list_files_in_prefix and unittests

---
+++ xetra/run.py
@@ -0,0 +1,24 @@
+""" Running the Xetra ETL Application """
+import logging
+import logging.config
+
+import yaml
+
+
+def main():
+    """
+    entry point to run the xetra ETL job
+    """
+    # Parsing YAML file
+
+    config_path = 'C:/Users/KandiVan/Documents/xetra_project/xetra/configs/xetra_report1_config.yml'
+    config = yaml.safe_load(open(config_path))
+    # configure logging
+    log_config = config['logging']
+    logging.config.dictConfig(log_config)
+    logger = logging.getLogger(__name__)
+    logger.info("This is a test.")
+
+
+if __name__ == '__main__':
+    main()
